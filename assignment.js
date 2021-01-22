cc90fc03f736cc3ed790497d74120e0370871b81
sheikhlimon <sheikhlimon404@gmail.com>
2021-01-22 17:54:11 +0600

    add kilometerToMeter solution

---
+++ assignment.js
@@ -0,0 +1,11 @@
+// https://github.com/sheikhlimon/javascript-problem-solving
+
+// First problem convert killometer to meter
+// To find the meters we have to multiply kilometers by 1000
+function kilometerToMeter(kilometers){
+    var meters = kilometers * 1000;
+    return meters;
+}
+
+
+
